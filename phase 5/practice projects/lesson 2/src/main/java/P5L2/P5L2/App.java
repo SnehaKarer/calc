dce32a29bc8735c34f28279a117da02bf87e977e
SnehaKarer <84636670+SnehaKarer@users.noreply.github.com>
2022-03-30 17:05:37 +0530

    Add files via upload

---
+++ phase 5/practice projects/lesson 2/src/main/java/P5L2/P5L2/App.java
@@ -0,0 +1,13 @@
+package P5L2.P5L2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
