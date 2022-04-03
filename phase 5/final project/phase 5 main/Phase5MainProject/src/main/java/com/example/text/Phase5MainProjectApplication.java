bb5c7c1eaedd8240ded1cc967a2e04182106d511
SnehaKarer <84636670+SnehaKarer@users.noreply.github.com>
2022-04-03 11:41:39 +0530

    Add files via upload
    Phase 5 Final project

---
+++ phase 5/final project/phase 5 main/Phase5MainProject/src/main/java/com/example/text/Phase5MainProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.text;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phase5MainProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phase5MainProjectApplication.class, args);
+	}
+
+}
